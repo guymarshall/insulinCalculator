c9faeb509c74b92e6ebb4f64ebf6b8fc75f848bb
Guy Marshall <guy@cslgroupservices.co.uk>
2022-08-11 12:45:16 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let carbsPer100 = document.getElementById("carbsPer100").value;
+let gramsOnScale = document.getElementById("gramsOnScale").value;
+let insulinRatio = document.getElementById("insulinRatio").value;
+
+document.getElementById("calculate").addEventListener("click", (carbsPer100, gramsOnScale, insulinRatio) => {
+    alert("clicked");
+});
