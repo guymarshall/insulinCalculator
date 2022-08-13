bd796a8e185eef29908a6b7cf2982b4c685ac522
Guy Marshall <guymarshall9@gmail.com>
2022-08-13 07:59:21 +0100

    Finish logic for function

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ let gramsOnScale = document.getElementById("gramsOnScale").value;
 let insulinRatio = document.getElementById("insulinRatio").value;
 
 document.getElementById("calculate").addEventListener("click", (carbsPer100, gramsOnScale, insulinRatio) => {
-    alert("clicked");
+    let carbsPerGram = carbsPer100 / 100;
+    let totalCarbs = carbsPerGram * gramsOnScale;
+    let insulinRequired = (insulinRatio / 10) * totalCarbs;
+
+    alert(`Total Carbs(g): ${totalCarbs}\nInsulin Required(units): ${insulinRequired}`);
 });
